738716495b99f7260e746dfc14f7aa984415d51f
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-06-26 04:53:08 +0900

    Refactor popup.js

--- popup.js
+++ popup.js
@@ -3,46 +3,61 @@ function setEnabled(){
     document.getElementById('disabledText').style.display = 'none';
     document.getElementById('disableButtonText').style.display = '';
     document.getElementById('EnableButtonText').style.display = 'none';
-    chrome.storage.local.set({'isEnabled': true}, ()=>{});
+    chrome.storage.local.set({'isEnabled': true}, null);
 }
+
 function setDisabled(){
     document.getElementById('enabledText').style.display = 'none';
     document.getElementById('disabledText').style.display = '';
     document.getElementById('disableButtonText').style.display = 'none';
     document.getElementById('EnableButtonText').style.display = '';
-    chrome.storage.local.set({'isEnabled': false}, ()=>{});
+    chrome.storage.local.set({'isEnabled': false}, null);
 }
 
-//init
-chrome.storage.local.get(["isEnabled", "selection", "locals"], (conf) => {
-    if(typeof conf.isEnabled == 'undefined') chrome.storage.local.set({'isEnabled': true}, ()=>{});
-    if(typeof conf.selection == 'undefined') chrome.storage.local.set({'selection': 'hant'}, ()=>{});
-    if(typeof conf.locals == 'undefined') chrome.storage.local.set({'locals': 'tw'}, ()=>{});
-
-    conf.isEnabled ? setEnabled() : setDisabled();
-    document.getElementById('selection').value = conf.selection
-    document.getElementById('locals').value = conf.locals
-
-});
-
-document.getElementById('isEnabled').addEventListener('click', () => {
-    chrome.storage.local.get("isEnabled", (conf) => {
-        if(conf.isEnabled) {
-            setDisabled();
-        } else {
-            setEnabled();
-        }
+function setDefaultValue(){
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(["isEnabled", "selection", "locals"], (conf) => {
+            if(typeof conf.isEnabled == 'undefined') chrome.storage.local.set({'isEnabled': true}, null);
+            if(typeof conf.selection == 'undefined') chrome.storage.local.set({'selection': 'hant'}, null);
+            if(typeof conf.locals == 'undefined') chrome.storage.local.set({'locals': 'tw'}, null);
+            resolve()
+        });
     });
-});
+}
 
-document.getElementById('selection').addEventListener('change', () => {
-    chrome.storage.local.set({
-        'selection': document.getElementById('selection').value
-    }, ()=>{});
-});
+function initConfigUi(){
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(["isEnabled", "selection", "locals"], (conf) => {
+            conf.isEnabled ? setEnabled() : setDisabled();
+            document.getElementById('selection').value = conf.selection
+            document.getElementById('locals').value = conf.locals
+        });
+    });
+}
 
-document.getElementById('locals').addEventListener('change', () => {
-    chrome.storage.local.set({
-        'locals': document.getElementById('locals').value
-    }, ()=>{});
-});
+(async () => {
+    await setDefaultValue();
+    await initConfigUi();
+
+    document.getElementById('isEnabled').addEventListener('click', () => {
+        chrome.storage.local.get("isEnabled", (conf) => {
+            if(conf.isEnabled) {
+                setDisabled();
+            } else {
+                setEnabled();
+            }
+        });
+    });
+
+    document.getElementById('selection').addEventListener('change', () => {
+        chrome.storage.local.set({
+            'selection': document.getElementById('selection').value
+        }, null);
+    });
+
+    document.getElementById('locals').addEventListener('change', () => {
+        chrome.storage.local.set({
+            'locals': document.getElementById('locals').value
+        }, null);
+    });
+} )()
